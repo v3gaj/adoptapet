3c71c712656ff2706b8d5e2aac4bdfd216d46a8a
v3gaj <juanvegaquesada@gmail.com>
2018-01-28 16:59:35 -0600

    Change Email Protocol

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   # CUSTOMS
 
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'tripcustomizers.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'tripcustomizers.com', protocol: 'http' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "gator4142.hostgator.com",
